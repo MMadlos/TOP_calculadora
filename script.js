d9e868e15dc70a4f0ad220e24ed0084c4f52e9e5
michael <michaelmadlos@hotmail.com>
2022-10-12 16:47:35 +0200

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,9 @@
+// Basic functions
+
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let operator = operate => operate;
+
