2afd0c241e3182ea90335802fa5fcb86d6e549ec
michael <michaelmadlos@hotmail.com>
2022-10-13 20:41:13 +0200

    Refactor function to select values to operate and display them

--- script.js
+++ script.js
@@ -8,31 +8,44 @@ const divide = (a, b) => a / b;
 let operator = operate => operate;
 
 // Display the number when clicking any button
+const resultadoActual = document.getElementById("resultadoActual");
+const tecladoNumerico = document.querySelectorAll("#tecladoNumerico > div")
+let numero = "";   
+let numbersToOperate = [];
 
-function displayNum() {
-    const resultadoActual = document.getElementById("resultadoActual");
-    const numSeleccionado = []
+// En función del índice (0 o 1) para el array numbersToOperate.
+function getNumber(index) {
+    tecladoNumerico.forEach(tecla => {
+        tecla.addEventListener("click", () => {
     
-    document
-        .querySelectorAll("#tecladoNumerico > div")
-        .forEach(tecla => {
-            tecla.addEventListener("click", () => {
-                // Si la tecla seleccionada no es un num, no hagas nada
-                if (tecla.textContent == "," || tecla.textContent == "=") {
-                    return
-                } else {
-                    // Crear una variable que recoja el número seleccionado
-                    let numero = tecla.textContent;
-                    
-                    // Añadir el número en el array
-                    numSeleccionado.push(numero);
-        
-                    // Mostrar array en pantalla
-                    resultadoActual.textContent = numSeleccionado.join("");
-                }
-            })
+            if (tecla.textContent == "=") {
+                return
+            }
+    
+            if (!numero.includes(",")) {
+                numero += tecla.textContent;
+                // resultadoActual.textContent = primerNumSelect.join("");
+    
+            } else if (tecla.textContent !== ",") {
+                numero += tecla.textContent;
+                // resultadoActual.textContent = primerNumSelect.join("");
+            } else {
+                alert("There's already a comma")
+            }
+            resultadoActual.textContent = numero;
+            numbersToOperate[index] = numero;
+    
+            console.log(numbersToOperate);
+            return numbersToOperate;
         })
+    })
 }
 
-displayNum();
 
+
+// TEST 
+
+const string = "12,99"
+const stringConverted = Number(string.replace(",", "."));
+
+console.log(stringConverted);
