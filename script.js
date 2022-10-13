390f664a9f671a1ae9ee538f4fccc061a1157565
michael <michaelmadlos@hotmail.com>
2022-10-13 12:49:22 +0200

    Add display numbers when clicked

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const divide = (a, b) => a / b;
 
 let operator = operate => operate;
 
+// Display the number when clicking any button
+
+function displayNum() {
+    const resultadoActual = document.getElementById("resultadoActual");
+    const numSeleccionado = []
+    
+    document
+        .querySelectorAll("#tecladoNumerico > div")
+        .forEach(tecla => {
+            tecla.addEventListener("click", () => {
+                // Si la tecla seleccionada no es un num, no hagas nada
+                if (tecla.textContent == "," || tecla.textContent == "=") {
+                    return
+                } else {
+                    // Crear una variable que recoja el número seleccionado
+                    let numero = tecla.textContent;
+                    
+                    // Añadir el número en el array
+                    numSeleccionado.push(numero);
+        
+                    // Mostrar array en pantalla
+                    resultadoActual.textContent = numSeleccionado.join("");
+                }
+            })
+        })
+}
+
+displayNum();
+
