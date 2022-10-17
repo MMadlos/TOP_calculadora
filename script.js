70c6bcdd8bbb1f5ae3ca80dbe531bc67c77ca5dd
michael <michaelmadlos@hotmail.com>
2022-10-17 18:36:12 +0200

    Add function to display and store two values for the operationto operate

--- script.js
+++ script.js
@@ -10,42 +10,69 @@ let operator = operate => operate;
 // Display the number when clicking any button
 const resultadoActual = document.getElementById("resultadoActual");
 const tecladoNumerico = document.querySelectorAll("#tecladoNumerico > div")
-let numero = "";   
-let numbersToOperate = [];
 
-// En función del índice (0 o 1) para el array numbersToOperate.
-function getNumber(index) {
-    tecladoNumerico.forEach(tecla => {
-        tecla.addEventListener("click", () => {
+let primerValor;
+let segundoValor;
+const suma = document.getElementById("suma")
+
+let wasClicked;
+
+suma.addEventListener("click", () => {
+    wasClicked = true;
+    resultadoActual.textContent += suma.textContent;
+})
+
+tecladoNumerico.forEach(tecla => {
+    tecla.addEventListener("click", () => {
+        if (wasClicked !== true) {
+            if (primerValor == undefined){
+                primerValor = "";                
+            }
+            if (primerValor[0] == undefined && tecla.textContent == "0"){
+                return ;                
+            }
     
             if (tecla.textContent == "=") {
                 return
             }
-    
-            if (!numero.includes(",")) {
-                numero += tecla.textContent;
-                // resultadoActual.textContent = primerNumSelect.join("");
-    
+            
+            if (!primerValor.includes(",")) {
+                primerValor += tecla.textContent;
+            
             } else if (tecla.textContent !== ",") {
-                numero += tecla.textContent;
-                // resultadoActual.textContent = primerNumSelect.join("");
+                primerValor += tecla.textContent;
             } else {
                 alert("There's already a comma")
             }
-            resultadoActual.textContent = numero;
-            numbersToOperate[index] = numero;
     
-            console.log(numbersToOperate);
-            return numbersToOperate;
-        })
+            resultadoActual.textContent = primerValor;
+
+        } else {
+            if (segundoValor == undefined){
+                segundoValor = "";                
+            }
+            if (segundoValor[0] == undefined && tecla.textContent == "0"){
+                return ;                
+            }
+    
+            if (tecla.textContent == "=") {
+                return
+            }
+            
+            if (!segundoValor.includes(",")) {
+                segundoValor += tecla.textContent;
+            
+            } else if (tecla.textContent !== ",") {
+                segundoValor += tecla.textContent;
+            } else {
+                alert("There's already a comma")
+            }
+    
+            resultadoActual.textContent += segundoValor;
+        }
     })
-}
+})
 
+// OPERACIÓN SIMPLE: SUMA
 
-
-// TEST 
-
-const string = "12,99"
-const stringConverted = Number(string.replace(",", "."));
-
-console.log(stringConverted);
+//TEST
