620284006659fa444b12d30b7af8204a60b9b521
michael <michaelmadlos@hotmail.com>
2022-10-18 14:39:03 +0200

    Add condition to do only 1 operation

--- script.js
+++ script.js
@@ -1,150 +1,83 @@
-// Basic functions
+// Operations
 
-const add = (a, b) => a + b;
+const add = (a, b = 0) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 let operator = operate => operate;
 
-// Display the number when clicking any button
-const resultadoActual = document.getElementById("resultadoActual");
+// Variables globales
+const pantalla = document.getElementById("resultadoActual");
 const tecladoNumerico = document.querySelectorAll("#tecladoNumerico > div")
 
-let primerValor;
-let segundoValor;
 const suma = document.getElementById("suma")
 const resultado = document.getElementById("resultado")
 
-let wasClicked;
-let value;
-let array = [];
-
-suma.addEventListener("click", () => { 
-    array.push(Number(value.replace(",", ".")));
-    value = "";
-    wasClicked = true;
-    resultadoActual.textContent += suma.textContent;
-    // console.log(array);
-
-})
-
-resultado.addEventListener("click", () => {
-    array.push(Number(value.replace(",", ".")));
-    wasClicked = false;
-
-    const resultadoOperacion = add(array[0], array[1])
-    resultadoActual.textContent += resultado.textContent + String(resultadoOperacion).replace(".", ",");
-    array = [];
-    value = "";
-})
+let wasClicked = false; // Para saber si ha pulsado "=" y empezar una nueva operación
+let value; // Para guardar el valor para la operación
+let valoresOperacion = []; // Para guardar los números a operar
+let resultadoOperacion; // Para 
 
 
+// FUNCIONES
+
+// Guarda los valores seleccionados en el array y los muestra en pantalla.
 tecladoNumerico.forEach(tecla => {    
     tecla.addEventListener("click", () => {
-        if (wasClicked !== true) {
-            if (value == undefined){
-                value = "";                
-            }
-            if (value[0] == undefined && tecla.textContent == "0"){
-                return ;                
-            }
-    
-            if (tecla.textContent == "=") {
-                return
-            }
-            
-            if (!value.includes(",")) {
-                value += tecla.textContent;
-            
-            } else if (tecla.textContent !== ",") {
-                value += tecla.textContent;
-            } else {
-                alert("There's already a comma")
-            }
-    
-            resultadoActual.textContent = value;
-
-        } else {
-            if (value == undefined){
-                value = "";                
-            }
-            if (value[0] == undefined && tecla.textContent == "0"){
-                return ;                
-            }
-    
-            if (tecla.textContent == "=") {
-                return
-            }
-            
-            if (!value.includes(",")) {
-                value += tecla.textContent;
-                resultadoActual.textContent += tecla.textContent
-            
-            } else if (tecla.textContent !== ",") {
-                value += tecla.textContent;
-                resultadoActual.textContent += tecla.textContent
-
-            } else {
-                alert("There's already a comma")
-            }
+        if (wasClicked){
+            pantalla.textContent = "";
+            wasClicked = false;
         }
+
+        if (value == undefined){
+            value = "";
+            pantalla.textContent = ""
+        }
+
+        //Evita que se añadan varios "0" al principio
+        if (value.length == 1 && tecla.textContent == "0") {
+            return
+        }
+
+        if (tecla.textContent == "=") {              
+            return                
+        }
+        
+        // Ver si tiene una coma para no poder añadir más comas
+
+        if(value.includes(",") && tecla.textContent == ",") {
+            alert("There's already a comma")
+            return
+        }
+
+        value += tecla.textContent;
+        pantalla.textContent += tecla.textContent;
     })
 })
 
 
-// ---BACK-UP-----------------------
-/* tecladoNumerico.forEach(tecla => {    
-    tecla.addEventListener("click", () => {
 
-        if (wasClicked !== true) {
-            if (primerValor == undefined){
-                primerValor = "";                
-            }
-            if (primerValor[0] == undefined && tecla.textContent == "0"){
-                return ;                
-            }
+// Ejemplo particular con la suma
+suma.addEventListener("click", () => { 
+    valoresOperacion.push(Number(value.replace(",", ".")));
+    pantalla.textContent += suma.textContent;
     
-            if (tecla.textContent == "=") {
-                return
-            }
-            
-            if (!primerValor.includes(",")) {
-                primerValor += tecla.textContent;
-            
-            } else if (tecla.textContent !== ",") {
-                primerValor += tecla.textContent;
-            } else {
-                alert("There's already a comma")
-            }
-    
-            resultadoActual.textContent = primerValor;
+    value = "";
 
-        } else {
-            if (segundoValor == undefined){
-                segundoValor = "";                
-            }
-            if (segundoValor[0] == undefined && tecla.textContent == "0"){
-                return ;                
-            }
-    
-            if (tecla.textContent == "=") {
-                return
-            }
-            
-            if (!segundoValor.includes(",")) {
-                segundoValor += tecla.textContent;
-            
-            } else if (tecla.textContent !== ",") {
-                segundoValor += tecla.textContent;
-
-            } else {
-                alert("There's already a comma")
-            }
-    
-            resultadoActual.textContent = segundoValor;
-        }
-    })
 })
- */
+
+// Funcinoalidad de la tecla "=" con la suma
+resultado.addEventListener("click", () => {
+    valoresOperacion.push(Number(value.replace(",", ".")));
+    resultadoOperacion = operator(add(valoresOperacion[0], valoresOperacion[1]))
+    pantalla.textContent += resultado.textContent + String(resultadoOperacion).replace(".", ",");
+
+    valoresOperacion = [];
+    value = "";
+    wasClicked = true;
+
+})
+
+
 //TEST
