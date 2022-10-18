7c1e9290cfae5a65fc3720652278c09225547f01
michael <michaelmadlos@hotmail.com>
2022-10-19 00:07:56 +0200

    Add substract and adding oepration functions

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 // Operations
 
-const add = (a = 0, b = 0) => a + b;
+/* const add = (a = 0, b = 0) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-let operator = operate => operate;
+let operator = operate => operate; */
 
 // Variables globales
 const pantalla = document.getElementById("resultadoActual");
 const tecladoNumerico = document.querySelectorAll("#tecladoNumerico > div")
 
-const suma = document.getElementById("suma")
-const resultado = document.getElementById("resultado")
-
 let wasClicked = false; // Para saber si ha pulsado "=" y empezar una nueva operación
 let value; // Para guardar el valor para la operación
 let valoresOperacion = []; // Para guardar los números a operar
-let resultadoOperacion; // Para 
+let resultadoOperacion; // Para mostrar en pantalla
 
 
 // FUNCIONES
@@ -36,7 +33,11 @@ tecladoNumerico.forEach(tecla => {
         }
 
         //Evita que se añadan varios "0" al principio
-        if (value.length == 1 && tecla.textContent == "0") {
+        if (value == "" && tecla.textContent == "0") {
+            pantalla.textContent = ""
+
+        }
+        if (value == "0" && tecla.textContent == "0") {
             return
         }
 
@@ -65,21 +66,54 @@ tecladoNumerico.forEach(tecla => {
 
 
 // Ejemplo particular con la suma
-suma.addEventListener("click", () => { 
-    if (pantalla.textContent.includes(suma.textContent)){
-        return
-    } else {
-        valoresOperacion.push(Number(value.replace(",", ".")));
-        pantalla.textContent += suma.textContent;
+const suma = document.getElementById("suma")
+
+// suma.addEventListener("click", () => { 
+//     if (pantalla.textContent.includes(suma.textContent)){
+//         return
+//     } else {
+//         valoresOperacion.push(Number(value.replace(",", ".")));
+//         pantalla.textContent = String(valoresOperacion[0]).replace(".", ",") + suma.textContent;
         
-        value = "";
-    }
+//         value = "";
+//     }
+// })
 
-    
+// FALTA GENERALIZAR AL RESTO DE OPERACIONES
+const add = (a = 0, b = 0) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
+let operator = operate => operate;
+
+const operadores = document.querySelectorAll(".operadores > div")
+let operacionSeleccionada;
+
+operadores.forEach(tecla => {
+    tecla.addEventListener("click", () => {
+        if(tecla.id == "suma"){
+            if (!pantalla.textContent.includes(suma.textContent)){
+                valoresOperacion.push(Number(value.replace(",", ".")));
+                pantalla.textContent = String(valoresOperacion[0]).replace(".", ",") + suma.textContent;                
+                value = "";
+                operacionSeleccionada = "suma"
+            }
+        }
+        if(tecla.id == "resta"){
+            if (!pantalla.textContent.includes(suma.textContent)){
+                valoresOperacion.push(Number(value.replace(",", ".")));
+                pantalla.textContent = String(valoresOperacion[0]).replace(".", ",") + resta.textContent;                
+                value = "";
+                operacionSeleccionada = "resta"
+            }
+        }
+    })
 })
 
 // Funcinoalidad de la tecla "=" con la suma
+const resultado = document.getElementById("resultado")
+
 resultado.addEventListener("click", () => {
     valoresOperacion.push(Number(value.replace(",", ".")));
 
@@ -93,20 +127,49 @@ resultado.addEventListener("click", () => {
         return 0;
     }
     
-
-    primerValor = getDecimalLength(valoresOperacion[0])
-    segundoValor = getDecimalLength(valoresOperacion[1])
-
     function getMaxLength(a, b = 0) {
         return (a > b) ? a : b;
     }
 
-    resultadoOperacion = add(valoresOperacion[0], valoresOperacion[1]).toFixed(getMaxLength(primerValor, segundoValor));
+    const primerValor = getDecimalLength(valoresOperacion[0])
+    const segundoValor = getDecimalLength(valoresOperacion[1])
+    const maxLength = getMaxLength(primerValor, segundoValor);
+
+    if (operacionSeleccionada == "suma"){
+        resultadoOperacion = add(valoresOperacion[0], valoresOperacion[1]).toFixed(maxLength);
+    }
+
+    if (operacionSeleccionada == "resta"){
+        resultadoOperacion = substract(valoresOperacion[0], valoresOperacion[1]).toFixed(maxLength);
+    }
+
 
     pantalla.textContent += resultado.textContent + String(resultadoOperacion).replace(".", ",");
-
     valoresOperacion = [];
     value = "";
     wasClicked = true;
 
+})
+
+// Botón borrar todo
+const btnAC = document.getElementById("btnAllClear")
+btnAC.addEventListener("click", () => {
+    value = undefined;
+    valoresOperacion = []
+    pantalla.textContent = "0";
+})
+
+// Botón borrar un dígito
+//!! Sólo funciona si no se ha utilizado el operador
+const btnErase = document.getElementById("btnErase")
+btnErase.addEventListener("click", () => {
+    if(value.length == 1){
+        value = undefined;
+        pantalla.textContent = "0"
+
+    } else {
+        value = value.slice(0, -1);
+        pantalla.textContent = pantalla.textContent.slice(0, -1);
+    
+    }
 })
